609e5fe6bd256e2dd7d60a72ff1d89f5fec20fde
Nick Ragaz <nick.ragaz@gmail.com>
2010-11-11 11:34:56 -0800

    Banish deprecation notice

--- lib/ole/file_system.rb
+++ lib/ole/file_system.rb
@@ -1,7 +1,9 @@
-warn <<-end
-Use of ole/file_system is deprecated. Use ole/storage (the file_system api
-is recommended and enabled by default).
-end
+# Thanks for the warning.
+#
+# warn <<-end
+# Use of ole/file_system is deprecated. Use ole/storage (the file_system api
+# is recommended and enabled by default).
+# end
 
 require 'ole/storage'
 
